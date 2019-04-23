4f8ab2ccf2bf73297644b8287abfd82c45e576e3
mliu433 <maggieqliu81@gmail.com>
2019-04-23 23:13:08 +1200

    Add toggle button to change website background colour

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react'
 import './Header.css'
 
+
 export default function Header() {
-    return (
-        <div className="header">
-            <h1 className='headerText'>How Old? 🤔</h1>
-        </div>
-    )
+return (
+    
+<div className="header">
+<h1 className='headerText' id="title">How Old? 🤔</h1>
+<div className="toggle-container">
+<input type="checkbox" id="switch" name="theme" onChange={change}  />
+<label htmlFor="switch"  >Toggle</label>
+
+</div>
+</div>
+)
+
+function change() {
+    const element =  document.getElementById("switch") as HTMLInputElement;  
+if(element !=null){
+    if(element.checked){
+        document.body.style.backgroundColor = "#FCFCFC";
+       const ele = document.getElementById("title");
+       if(ele!=null){
+        ele.style.color = "#red";
+       }
+    }else if(!element.checked){
+        document.body.style.backgroundColor = "#323639";
+    }
+}
+   
+   
+}
+
 }
